ab110d91d657bd81edf1a0bf6f9d7994f2d6e05c
Lucia Cabrera <cabreramartinezl@yahoo.com.ar>
2023-08-25 15:11:26 -0300

    Cambios

--- Transporte/frontend/src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Footer =(props) => {
-    return(
-       <footer>
-        <p>Derechos reservados.Cyberseguridad @2023</p>
-       </footer>
-    );
-}
-export default Footer;
--- Transporte/frontend/src/pages/contactoPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const contactoPage =(props)=> {
-    return(
-        <div>contactoPage</div>
-    );
-}
-export default contactoPage;

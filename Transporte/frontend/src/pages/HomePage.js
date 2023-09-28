c86a752cc55135d2127cc5a3e8b2a7c6a86f74bc
Lucia Cabrera <cabreramartinezl@yahoo.com.ar>
2023-09-28 15:13:12 -0300

    Tarea 2 cambios

--- Transporte/frontend/src/pages/HomePage.js
+++ Transporte/frontend/src/pages/HomePage.js
@@ -6,16 +6,17 @@ const HomePage =(props)=> {
         <div className="homeimg">
             <img src='' alt=''/>
         </div>
-         <div className="colum">
-            <div className="bienvenidos left">
+         <div className="columnas">
+            <div className="bienvenidos">
                 <h2>Bienvenidos</h2>
                 <p> Somos Cyberseguridad LCM una empresa de seguridad informatica,creamos esta pagina para concientizar y ayudar al usuario ante un delito de Cyberseguridad.En esta plataforma encontraras toda la informacion necesaria y tambien podras dejarnos tus dudas</p>
             </div>
-            <div className="testimonios right"></div>
+            <div className="testimonios">
             <h2>Testimonios</h2>
             <div className="testimonio">
                 <span className="cita">Excelente, me ayudaron en una emergencia</span>
                 <span className="autor">Gabriela Muñiz - victima de cyber estafas</span>
+            </div>
             </div>
          </div>
        </main>

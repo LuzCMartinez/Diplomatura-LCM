7f855bab3c8015f01d3fa9dc9cf0ef514f4fb4ad
Lucia Cabrera <cabreramartinezl@yahoo.com.ar>
2023-10-19 15:28:19 -0300

    Cambios tarea

--- Transporte/backend/routes/admin/login.js
+++ Transporte/backend/routes/admin/login.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('admin/login', { layout: 'admin/layout' });
+  res.render('admin/login', { 
+    layout: 'admin/layout'
+   });
 });
 
 module.exports = router;

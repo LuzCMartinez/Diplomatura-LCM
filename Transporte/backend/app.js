5ceb97a38f04dd8b653340f7a5e5f52645aff2a0
Lucia Cabrera <cabreramartinezl@yahoo.com.ar>
2023-11-16 16:55:21 -0300

    Cambios

--- Transporte/backend/app.js
+++ Transporte/backend/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+var pool = require('./models/bd');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var  loginRouter = require('./routes/admin/login');
@@ -22,6 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter)
+
+//selec
+pool.query('select * from empleados').then(function
+  (resultados){
+    console.log(resultados)
+  });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

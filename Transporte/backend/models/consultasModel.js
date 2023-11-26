cf13029cc6ee36d6b22dd35bdabdeb5fbc775cfc
Lucia Cabrera <cabreramartinezl@yahoo.com.ar>
2023-11-26 00:02:21 -0300

    cambios

--- Transporte/backend/models/consultasModel.js
+++ Transporte/backend/models/consultasModel.js
@@ -15,4 +15,23 @@ async function insertConsulta (obj){
         throw error;
     }
 }
-module.exports ={getConsultas,insertConsulta }
+async function deleteConsultasById(id){
+    var query = "delete from consultas where id = ?";
+    var rows = await pool.query(query,[id]);
+    return rows;
+}
+async function getConsultasById (id){
+    var query = "select * from consultas where id = ?";
+    var rows = await pool.query (query, [id]);
+    return rows [0];
+}
+async function modificarConsultasById(obj,id){
+    try{
+        var query = "update consultas set ? where id =?";
+        var rows = await pool.query (query,[obj,id]);
+        return rows; 
+    }catch (error){
+        throw error;
+    }
+}
+module.exports ={getConsultas,insertConsulta, deleteConsultasById, getConsultasById, modificarConsultasById}
--- Transporte/backend/routes/admin/consultas.js
+++ Transporte/backend/routes/admin/consultas.js
@@ -16,4 +16,52 @@ router.get('/agregar',(req,res,next)=>{
         layout:'admin/layout'
     });
 });
+router.get('/modificar/:id', async (req,res,next)=>{
+  let id = req.params.id;
+  let consulta = await consultasModel.getConsultasById(id);
+  res.render('admin/modificar',{
+    layout: 'admin/layout',
+    consulta
+  });
+});
+router.post('/modificar', async (req,res,next)=>{
+    try{
+        let obj={
+            pregunta:req.body.pregunta,
+            respuesta: req.body.respuesta
+        }
+        await consultasModel.modificarConsultasById(obj, req.body.id);
+        res.redirect('/admin/consultas');
+    }catch(error){
+        console.log(error)
+        res.render('admin/modificar',{
+            layout:'admin/layout',
+            error: true, message: 'No se modifico la consulta'
+        })
+    }
+})
+router.get('/eliminar/:id', async (req,res,next)=>{
+    var id = req.params.id;
+    await consultasModel.deleteConsultasById(id);
+    res.redirect('/admin/consultas')
+});
+router.post('/agregar',async(req,res,next)=>{
+    try{
+        if (req.body.pregunta!="" && req.body.respuesta!=""){
+            await consultasModel.insertConsulta(req.body);
+            res.redirect('/admin/consultas')
+        }else{
+            res.render('admin/agregar',{
+                layout:'admin/layout',
+                error: true, message:'Todos los campos son requeridos'
+            })
+        }
+    }catch (error){
+        console.log(error)
+        res.render('admin/agregar',{
+            layout: 'admin/layout',
+            error:true, message:'No se cargo la novedad'
+        });
+    }
+})
 module.exports =router;
